8312e61bcd3f0650d3ac07278ebbc7d985a8caaa
Shreyasnalle <shreyas.nalle7@gmail.com>
2026-08-27 14:12:55 +0530

    HI large ds downloaded

--- model/get_dataset.py
+++ model/get_dataset.py
@@ -3,21 +3,17 @@ import pandas as pd
 import kagglehub
 
 DATASET_HANDLE = "ealtman2019/ibm-transactions-for-anti-money-laundering-aml"
+KAGGLE_CACHE_DIR = os.path.expanduser("~/.cache/kagglehub/datasets/ealtman2019/ibm-transactions-for-anti-money-laundering-aml/versions/8")
+SAFE_NROWS = 15_000_000
 
-def load_aml_dataset(file_name = "HI-Medium_Trans.csv"):
-    dataset_dir = kagglehub.dataset_download(DATASET_HANDLE)
-    
-    csv_file_path = os.path.join(dataset_dir, file_name)
-    if not os.path.exists(csv_file_path):
-        for root, dirs, files in os.walk(dataset_dir):
-            if file_name in files:
-                csv_file_path = os.path.join(root, file_name)
-                break
-                
-    if not os.path.exists(csv_file_path):
-        raise FileNotFoundError(f"CSV file '{file_name}' not found in {dataset_dir}")
-        
-    print(f"[*] Reading '{csv_file_path}' into pandas DataFrame...")
-    df = pd.read_csv(csv_file_path)
-    print(f"[+] Successfully loaded DataFrame shape: {df.shape}")
+def load_aml_dataset(file_name = "HI-Large_Trans.csv") :
+    csv_file_path = os.path.join(KAGGLE_CACHE_DIR, file_name)
+    if not os.path.exists(csv_file_path) :
+        dataset_dir = kagglehub.dataset_download(DATASET_HANDLE, path = file_name)
+        if os.path.isfile(dataset_dir) :
+            csv_file_path = dataset_dir 
+        else :
+            csv_file_path = os.path.join(dataset_dir, file_name) 
+
+    df = pd.read_csv(csv_file_path, nrows=SAFE_NROWS)
     return df
