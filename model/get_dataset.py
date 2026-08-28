8307ae1b0ddc850497749bc6954b6cb0fb71d8aa
Shreyasnalle <shreyas.nalle7@gmail.com>
2026-08-29 00:13:27 +0530

    gnie trained model

--- model/get_dataset.py
+++ model/get_dataset.py
@@ -4,7 +4,7 @@ import kagglehub
 
 DATASET_HANDLE = "ealtman2019/ibm-transactions-for-anti-money-laundering-aml"
 KAGGLE_CACHE_DIR = os.path.expanduser("~/.cache/kagglehub/datasets/ealtman2019/ibm-transactions-for-anti-money-laundering-aml/versions/8")
-SAFE_NROWS = 15_000_000
+SAFE_NROWS = 22_000_000
 
 def load_aml_dataset(file_name = "HI-Large_Trans.csv") :
     csv_file_path = os.path.join(KAGGLE_CACHE_DIR, file_name)
